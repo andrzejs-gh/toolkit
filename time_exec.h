281a69007ac66c2e08530d49266924e5d8fcbcdf
andrzejs-gh <249625581+andrzejs-gh@users.noreply.github.com>
2026-07-09 21:27:56 +0200

    ...

--- time_exec.h
+++ time_exec.h
@@ -4,20 +4,37 @@
 #include <time.h>
 #include <stdio.h>
 
-#define GET_EXEC_TIME(func_or_code) do { \
-                                        struct timespec start_ts, end_ts; \
-                                        clock_gettime(CLOCK_MONOTONIC, &start_ts); \
-                                        func_or_code;  \
-                                        clock_gettime(CLOCK_MONOTONIC, &end_ts); \
-                                        double start = start_ts.tv_sec + start_ts.tv_nsec * 1e-9; \
-                                        double end = end_ts.tv_sec + end_ts.tv_nsec * 1e-9;
+#define GET_EXEC_TIME(func_or_code) \
+do { \
+    struct timespec T_EXEC_MACRO_start_ts, T_EXEC_MACRO_end_ts; \
+    clock_gettime(CLOCK_MONOTONIC, &T_EXEC_MACRO_start_ts); \
+    func_or_code;  \
+    clock_gettime(CLOCK_MONOTONIC, &T_EXEC_MACRO_end_ts); \
+    double T_EXEC_MACRO_start = T_EXEC_MACRO_start_ts.tv_sec + \
+                                T_EXEC_MACRO_start_ts.tv_nsec * 1e-9; \
+    double T_EXEC_MACRO_end = T_EXEC_MACRO_end_ts.tv_sec + \
+                              T_EXEC_MACRO_end_ts.tv_nsec * 1e-9;
 
-#define TIME_PRINT(func_or_code) GET_EXEC_TIME(func_or_code) \
-                                 printf("t = %.9f s\n", end - start); \
-                                 } while (0);
+#define TIME_PRINT(func_or_code) \
+    GET_EXEC_TIME(func_or_code) \
+    printf("t = %.9f s\n", T_EXEC_MACRO_end - \
+                           T_EXEC_MACRO_start); \
+} while (0);
 
-#define TIME_STORE(func_or_code, variable) GET_EXEC_TIME(func_or_code) \
-                                           variable = end - start; \
-                                           } while (0);
+#define TIME_STORE(variable, func_or_code) \
+    GET_EXEC_TIME(func_or_code) \
+    variable = T_EXEC_MACRO_end - \
+               T_EXEC_MACRO_start; \
+} while (0);
+
+#define REPEAT(N, func_or_code) \
+do { \
+    for (size_t T_EXEC_MACRO_i = 0; T_EXEC_MACRO_i < N; T_EXEC_MACRO_i++) \
+    { \
+        func_or_code; \
+    } \
+} while (0);
+
+
 
 #endif
